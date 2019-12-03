dd9f6fbec70b4c33b381fd50790372d447be09d0
paul <paul@10.31.208.105>
2019-12-03 18:42:41 +0100

    New Commit

---
+++ src/fr/adresses/MainApp.java
@@ -0,0 +1,24 @@
+package fr.adresses;
+
+
+import fr.adresses.classes.Person;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+	private Stage primaryStage;
+	private ObservableList<Person> personData = FXCollections.observableArrayList();
+	
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AlcoolApp");
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
