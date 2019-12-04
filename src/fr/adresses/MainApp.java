2176cc46a435baf045328ae0ed58a57fbe7cae1f
paul <paul@10.31.208.105>
2019-12-04 14:43:38 +0100

    Graphic commit

--- src/fr/adresses/MainApp.java
+++ src/fr/adresses/MainApp.java
@@ -1,10 +1,18 @@
 package fr.adresses;
 
 
+import java.io.IOException;
+
+import fr.adresses.classes.LineChartsValues;
 import fr.adresses.classes.Person;
+import fr.adresses.views.graphicOverviewController;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -41,6 +49,36 @@ public class MainApp extends Application {
 	 * 
 	 * 
 	 */
+	
+	
+	public boolean showGraphic(Person person) {
+		try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/GraphicOverview.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle(person.getFirstName());
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            LineChartsValues values = new LineChartsValues(person);
+            
+            graphicOverviewController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(person);
+            controller.setChart(values.getChart());
+
+            dialogStage.showAndWait();
+            return controller.isOkClicked();
+            
+	       } catch (IOException e) {
+	           e.printStackTrace();
+	           return false;
+	       }
+	}
 	
 	public static void main(String[] args) {
 		launch(args);
