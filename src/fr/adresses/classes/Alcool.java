d2a0f81a8cffb6400132f13215423e9dae68c7b8
paul <paul@192.168.0.13>
2019-12-07 20:31:34 +0100

    2.7

--- src/fr/adresses/classes/Alcool.java
+++ src/fr/adresses/classes/Alcool.java
@@ -1,49 +1,65 @@
 package fr.adresses.classes;
 
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Alcool {
 	private final StringProperty alcoolName;
-	private IntegerProperty quantity;
 	private final IntegerProperty degree;
+	private final DoubleProperty quantity;
 	
 	 public Alcool() {
-	        this(null, null);
+	        this(null, 0);
 	    }
 	 
-	public Alcool(String alcoolName, String dose) {
-
-		this.alcoolName = new SimpleStringProperty("rhum");
-		this.degree = new SimpleIntegerProperty(3);
-		this.quantity = new SimpleIntegerProperty(0);
+	public Alcool(String alcoolName, int degree) {
+		this.alcoolName = new SimpleStringProperty(alcoolName);
+		this.degree = new SimpleIntegerProperty(degree);
+		this.quantity = new SimpleDoubleProperty(0);
+		
 	}
 
 	
 	
 	public String getAlcoolName() {
-	        return alcoolName.get();
-	    }
+		return alcoolName.get();
+	}
 
-	    public void setAlcoolName(String alcoolName) {
-	        this.alcoolName.set(alcoolName);
-	    }
+	public void setAlcoolName(String alcoolName) {
+	    this.alcoolName.set(alcoolName);
+	}
 	    
-	    public StringProperty alcoolNameProperty() {
-	        return alcoolName;
-	    }
+	public StringProperty alcoolNameProperty() {
+		return alcoolName;
+	}
 
-	    public int getDose() {
-	        return degree.get();
-	    }
+	public int getDegree() {
+		return degree.get();
+	}
 
-	    public void setDose(int dose) {
-	        this.degree.set(dose);
-	    }
+	public void setDegree(int dose) {
+	    this.degree.set(dose);
+	}
 	    
-	    public IntegerProperty doseProperty() {
-	        return degree;
-	    }
+	public IntegerProperty degreeProperty() {
+		return degree;
+	}
+	
+	public double getQuantity() {
+		return quantity.get();
+	}
+
+	public void setQuantity(double dose) {
+	    this.quantity.set(dose);
+	}
+	    
+	public DoubleProperty quantityProperty() {
+		return quantity;
+	}
+	
+	
 }
