fd1ab5e744b504defb3bd88a55356155c30e263e
Flassch Gil <Flassch Gil@LAPTOP-N6NICQ3V>
2019-12-10 22:05:14 +0100

    gil commit

--- src/fr/adresses/views/MainOverviewController.java
+++ src/fr/adresses/views/MainOverviewController.java
@@ -9,7 +9,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class MainOverviewController {
-	
+
 	@FXML
 	private TableView<Person> personTable;
 	@FXML
@@ -18,15 +18,15 @@ public class MainOverviewController {
 	private TableColumn<Person, String> lastNameColumn;
 	@FXML
 	private TableColumn<Person, String> weightColumn;
-	
-    @FXML
-    private TableView<Alcool> alcoolTable;
-    @FXML
-    private TableColumn<Alcool, String> alcoolNameColumn;
-    @FXML
-    private TableColumn<Alcool, String> degreeColumn;
-    
-    @FXML
+
+	@FXML
+	private TableView<Alcool> alcoolTable;
+	@FXML
+	private TableColumn<Alcool, String> alcoolNameColumn;
+	@FXML
+	private TableColumn<Alcool, String> degreeColumn;
+
+	@FXML
 	private TableView<Alcool> selectedAlcoolTable;
 	@FXML
 	private TableColumn<Alcool, String> selectedAlcoolColumn;
@@ -34,10 +34,16 @@ public class MainOverviewController {
 	private TableColumn<Alcool, String> quantityColumn;
 
 	@FXML
+	private Label firstNameLabel;
+	private Label lastNameLabel;
+	private Label weightLabel;
+	private Label alcoolNameLabel;
+	private Label degreeLabel;
 	private Label selectedAlcoolLabel;
-	
+	private Label quantityLabel;
+
 	private MainApp mainApp;
-	
+
 	@FXML
 	private void initializePerson() {
 		// Initialize the person table with the two columns.
@@ -52,24 +58,71 @@ public class MainOverviewController {
 				(observable, oldValue, newValue) -> showPersonDetails(newValue));
 	}
 
+	@FXML
+	private void initializeAlcool() {
+		// Initialize the person table with the two columns.
+		alcoolNameColumn.setCellValueFactory(cellData -> cellData.getValue().alcoolNameProperty());
+		degreeColumn.setCellValueFactory(cellData -> cellData.getValue().degreeProperty());
+
+		// Clear alcool details.
+		showAlcoolDetails(null);
+
+		alcoolTable.getSelectionModel().selectedItemProperty().addListener(
+				(observable, oldValue, newValue) -> showAlcoolDetails(newValue));
+	}
+
+	@FXML
+	private void initializeSelectedAlcool() {
+		// Initialize the person table with the two columns.
+		selectedAlcoolColumn.setCellValueFactory(cellData -> cellData.getValue().selectedAlcoolProperty());
+		quantityColumn.setCellValueFactory(cellData -> cellData.getValue().quantityProperty());
+
+		// Clear alcool selected details.
+		showSelectedAlcoolDetails(null);
+
+		selectedAlcoolTable.getSelectionModel().selectedItemProperty().addListener(
+				(observable, oldValue, newValue) -> showSelectedAlcoolDetails(newValue));
+	}
+
 	private void showPersonDetails(Person person) {
 		if (person != null) {
 			firstNameLabel.setText(person.getFirstName());
 			lastNameLabel.setText(person.getLastName());
 			weightLabel.setText(Double.toString(person.getWeight()));
-			sexLabel.setText(person.getSex());
 
 		} else {
 			firstNameLabel.setText("");
 			lastNameLabel.setText("");
 			weightLabel.setText("");
-			sexLabel.setText("");
+		}
+	}
+
+	private void showAlcoolDetails(Alcool alcool) {
+		if (alcool != null) {
+			alcoolNameLabel.setText(alcool.getAlcoolName());
+			degreeLabel.setText(Double.toString(alcool.getDegree()));
+
+		} else {
+			alcoolNameLabel.setText("");
+			degreeLabel.setText("");
+		}
+	}
+
+	private void showSelectedAlcoolDetails(Alcool alcool) {
+		if (alcool!= null) {
+			selectedAlcoolLabel.setText(alcool.getSelectedAlcool());
+			quantityLabel.setText(alcool.getQuantity());
+		} else {
+			selectedAlcoolLabel.setText("");
+			quantityLabel.setText("");
 		}
 	}
 	public void setMainApp(MainApp mainApp) {
 		this.mainApp = mainApp;
 		personTable.setItems(mainApp.getPersonData());
+		alcoolTable.setItems(mainApp.getAlcoolData());
+		selectedAlcoolTable.setItems(mainApp.getSelectedAlcoolData());
 	}
-	
-	
+
+
 }
