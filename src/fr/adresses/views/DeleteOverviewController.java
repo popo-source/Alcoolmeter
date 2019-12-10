591c68c2e5d67301d058a56d2c0085cd0a0e55a9
Lucas <Lucas@Lucas>
2019-12-10 23:37:31 +0100

    j'ai pas tout compris mais c'est un début

--- src/fr/adresses/views/DeleteOverviewController.java
+++ src/fr/adresses/views/DeleteOverviewController.java
@@ -1,8 +1,32 @@
 package fr.adresses.views;
 
-public class DeleteOverviewController {
-	
-	int i;
-	
+import fr.adresses.MainApp;
+import fr.adresses.classes.Alcool;
+import fr.adresses.classes.Person;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 
+public class DeleteOverviewController {
+	private boolean okClick=false;
+	private boolean noClick=false;
+	private Stage dialogStage;
+	@FXML
+	private void handleOk() 
+	
+	{
+		okClick=true;
+		//int selectedIndex = TableColumn.getSelectionModel().getSelectedIndex();
+		dialogStage.close();
+		
+	}
+	@FXML
+	private void handleNo() 
+	{
+		noClick=true;
+		dialogStage.close();
+		
+	}
 }
