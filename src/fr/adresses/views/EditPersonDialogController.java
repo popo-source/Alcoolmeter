bc3c90480c666fa046b05b7166482120997e58bf
Flassch Gil <Flassch Gil@LAPTOP-N6NICQ3V>
2019-12-10 21:43:23 +0100

    gil controller edit

--- src/fr/adresses/views/EditPersonDialogController.java
+++ src/fr/adresses/views/EditPersonDialogController.java
@@ -22,10 +22,12 @@ public class EditPersonDialogController {
 	public EditPersonDialogController() {
 	}
 
+	private MainApp mainApp;
+
 	public void setMainApp(MainApp mainApp) {
 		this.mainApp = mainApp;
 	}
-	
+
 	private boolean isInputValid() {
 		String errorMessage = "";
 
--- src/fr/adresses/views/EditQuantityDialogController.java
+++ src/fr/adresses/views/EditQuantityDialogController.java
@@ -10,8 +10,51 @@ import fr.adresses.classes.Alcool;
 public class EditQuantityDialogController {
 
 	@FXML
-	private Label quantityLabel;
+	private Textfield quantityField;
 
 	public EditQuantityDialogController() {
+	}
+	
+	private MainApp mainApp;
+	
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp;
+	}
+	
+	private boolean isInputValid() {
+		String errorMessage = "";
+
+		if (firstNameField.getText() == null || firstNameField.getText().length() == 0) {
+			errorMessage += "No valid first name!\n";
+		}
+		if (lastNameField.getText() == null || lastNameField.getText().length() == 0) {
+			errorMessage += "No valid last name!\n";
+		}
+
+		if (weightField.getText() == null || weightField.getText().length() == 0) {
+			errorMessage += "No valid weight!\n";
+		} else {
+			// try to parse the postal code into an int.
+			try {
+				Integer.parseInt(weightField.getText());
+			} catch (NumberFormatException e) {
+				errorMessage += "No valid weight (must be an integer)!\n";
+			}
+		}
+
+		if (errorMessage.length() == 0) {
+			return true;
+		} else {
+			// Show the error message.
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.initOwner(dialogStage);
+			alert.setTitle("Invalid Fields");
+			alert.setHeaderText("Please correct invalid fields");
+			alert.setContentText(errorMessage);
+
+			alert.showAndWait();
+
+			return false;
+		}
 	}
 }
