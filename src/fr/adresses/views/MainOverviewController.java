91c8fb4e469b0c2a0a5572d7c66e533c3fc62ae2
paul <paul@10.31.208.105>
2019-12-12 10:21:58 +0100

    jijijijii

--- src/fr/adresses/views/MainOverviewController.java
+++ src/fr/adresses/views/MainOverviewController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TableView.TableViewSelectionModel;
 import javafx.scene.control.Alert.AlertType;
 
 public class MainOverviewController {
