67c14e074b6f093271d6c619c1371baa1a856048
Lucas <Lucas@10.31.207.219>
2019-12-13 09:44:02 +0100

    avec fxml c'est mieux

--- src/fr/adresses/views/NewOverviewController.java
+++ src/fr/adresses/views/NewOverviewController.java
@@ -1,22 +1,25 @@
 package fr.adresses.views;
 
+import javafx.fxml.FXML;
 import javafx.stage.Stage;
 
 public class NewOverviewController {
 private int click=0;
 private Stage DialogStage;
-	
+	@FXML
 	private void handlePerson() 
 	{
 		click=1;
 		DialogStage.close();
 	}
+	@FXML
 	private void handleAlcool() 
 	{
 		click=2;
 		DialogStage.close();
-		//
+		
 	}
+	@FXML
 	private void handleCancel() 
 	{
 		click=3;
