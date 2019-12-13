937dc975223ce70bffc797f469f9ac1d023fb2d2
Lucas <Lucas@10.31.207.219>
2019-12-13 09:38:23 +0100

    Stage actu

--- src/fr/adresses/views/NewOverviewController.java
+++ src/fr/adresses/views/NewOverviewController.java
@@ -1,14 +1,27 @@
 package fr.adresses.views;
 
-public class NewOverviewController {
+import javafx.stage.Stage;
 
+public class NewOverviewController {
+private int click=0;
+private Stage DialogStage;
 	
 	private void handlePerson() 
 	{
-		
+		click=1;
+		DialogStage.close();
 	}
 	private void handleAlcool() 
 	{
-		//
+		click=2;
+		DialogStage.close();
+	}
+	private void handleCancel() 
+	{
+		click=3;
+		DialogStage.close();
+	}
+	public void setDialogStage(Stage dialogStage) {
+		this.DialogStage = dialogStage;
 	}
 }
